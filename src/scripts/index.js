e5c1079284980a92c11c65b1f6ca11698e3652a0
Everest Shi <shi.everest@gmail.com>
2024-01-15 20:46:21 -0800

    test merge

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // JavaScript Document
 
 // Responsive Menu - Dropdown
@@ -9,37 +9,35 @@ const navLinks = document.querySelectorAll(".nav-item");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
+
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   });
-});document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.you-should-know-carousel', {
-        slidesPerView: 'auto',
-        spaceBetween: 20,
-        navigation: {
-            nextEl: '.you-should-know-chevron-right',
-            prevEl: '.you-should-know-chevron-left',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-
-    // Add event listener to the form to change the active slide
-    var carouselSelectorForm = document.getElementById('carouselSelectorForm');
-    carouselSelectorForm.addEventListener('change', function (event) {
-        var selectedValue = event.target.value;
-        if (selectedValue) {
-            swiper.slideTo(selectedValue - 1); // Swiper uses zero-based index
-        }
-    });
 });
-  
-  
-  
-  
-  
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".you-should-know-carousel", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".you-should-know-chevron-right",
+      prevEl: ".you-should-know-chevron-left",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // Add event listener to the form to change the active slide
+  var carouselSelectorForm = document.getElementById("carouselSelectorForm");
+  carouselSelectorForm.addEventListener("change", function (event) {
+    var selectedValue = event.target.value;
+    if (selectedValue) {
+      swiper.slideTo(selectedValue - 1); // Swiper uses zero-based index
+    }
+  });
+});
